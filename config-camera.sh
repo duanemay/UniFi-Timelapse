37744b206cea874613893d070f36dee3557a7d7a
Duane May <duanemay@users.noreply.github.com>
2022-02-12 14:10:22 -0500

    Update config-camera.sh
    Better comment

--- config-camera.sh
+++ config-camera.sh
@@ -1,9 +1,9 @@
 #!/bin/false
 # shellcheck shell=bash
+# shellcheck disable=SC2034 ## This file is sourced by multiple scripts
 
+# Add each camera with a unique name and its cooresponding snap.jpeg URL here
 CAMS["Voordeur"]="http://10.1.1.244/snap.jpeg"
-
-# if you use other CAMS add them here
 #CAMS["Back Door"]="http://192.1.1.2/snap.jpeg"
 #CAMS["Driveway"]="http://192.1.1.3/snap.jpeg"
 #CAMS["Back Garden"]="http://192.1.1.4/snap.jpeg"
